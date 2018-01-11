c875b4163bd577ee9c7353eac17e2929c54f4649
Gnedov Pavel <pavel@gnedov.info>
2018-01-12 02:37:29 +0700

    Изменена главная вьюшка
    * Получение вложенных компонентов с помощью функций
    * При выборе списка задач передача id в дерево задач

--- app/MainView.js
+++ app/MainView.js
@@ -14,17 +14,46 @@ Ext.define('Gtd.MainView', {
 
 	layout: 'border',
 	
-	items: [
-		{
-			region: 'west',
-			xtype: 'Gtd.view.TaskList',
-			collapsible: true,
-			width: 200
-		},
-		{
-			region: 'center',
-			xtype: 'Gtd.view.TaskTree'
+	initComponent: function() {
+		this.items = [
+			this.getTaskList(),
+			this.getTaskTree()
+		];
+		this.callParent();
+		this.initListeners();
+	},
+	
+	getTaskList: function() {
+		if (this.taskList) {
+			return this.taskList;
 		}
-	],
+		this.taskList = Ext.create('Gtd.view.TaskList', {
+			width: 200,
+			region: 'west',
+			collapsible: true
+		});
+		return this.taskList;
+	},
+	
+	getTaskTree: function() {
+		if (this.taskTree) {
+			return this.taskTree;
+		}
+		this.taskTree = Ext.create('Gtd.view.TaskTree', {
+			region: 'center'
+		});
+		return this.taskTree;
+	},
+	
+	initListeners: function() {
+		var list = this.getTaskList(); // Gtd.view.TaskList
+		list.on('select', this.onSelectTaskList, this);
+	},
+	
+	onSelectTaskList: function(gridpanel, record) {
+		var id = record.get('id');
+		var tree = this.getTaskTree(); // Gtd.view.TaskTree
+		tree.setListId(id);
+	},
 	
 });
