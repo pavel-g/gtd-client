2fe5ca060ea55a12487628bbfe02f555cc3555c6
Gnedov Pavel <pavel@gnedov.info>
2018-01-12 02:16:19 +0700

    Подключено хранилище к дереву задач

--- app/view/TaskTree.js
+++ app/view/TaskTree.js
@@ -2,8 +2,36 @@ Ext.define('Gtd.view.TaskTree', {
 	
 	extend: 'Ext.tree.Panel',
 	
+	requires: [
+		'Gtd.store.TaskTree',
+	],
+	
 	alias: 'widget.Gtd.view.TaskTree',
 	
-	title: 'Дерево задач'
+	title: 'Дерево задач',
+	
+	rootVisible: false,
+	
+	initComponent: function() {
+		this.store = Ext.create('Gtd.store.TaskTree');
+		this.callParent();
+	},
+	
+	setListId: function(listId) {
+		var store = this.getStore();
+		store.listId = listId;
+		var proxy = store.getProxy();
+		proxy.setExtraParam('list_id', listId);
+		store.load();
+	},
+	
+	getListId: function() {
+		var store = this.getStore();
+		if (typeof store.listId !== 'undefined') {
+			return store.listId;
+		} else {
+			return null;
+		}
+	},
 	
 });
